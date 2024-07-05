e83882657be8deb4b21201b5ef3d36cf2995f20c
jrealm <jrealm@gmail.com>
2024-07-05 12:16:23 +0800

    init

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php //>
+
+const APP_DATA = APP_HOME . 'data/';
+const APP_LOG = APP_HOME . 'logs/';
+const FILES_HOME = APP_HOME . 'www/files/';
+const PRIV_FILES_HOME = APP_HOME . 'files/';
+
+const DB_NAME = 'pgsql:dbname=demo_db';
+const DB_USER = 'demo';
+const DB_PASSWORD = null;
+
+const PACKAGES = [
+    'matrix-platform/backend-adminlte',
+];
